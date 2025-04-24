e91928211e736dd2ba603ff53e4dce9f8b18289d
Peter-White <pwhitedeveloper@gmail.com>
2025-04-24 18:14:43 -0500

    anime / genre models added

--- anime/models.py
+++ anime/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Anime(models.Model):
+    title = models.CharField(max_length=200)
+    j_title = models.CharField(max_length=200)
+    synopsis = models.TextField()
+    runtime = models.IntegerField()
+    type = models.CharField(max_length=15)
+    release_date = models.DateField()
+    duration = models.IntegerField()
+    rating = models.CharField(max_length=7)
+    
+    class Meta:
+        db_table = 'anime'
+    
+    def __str__(self):
+        return self.title + " (" + self.release_date + ")"
+    
+class Genre(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    description = models.TextField()
+    anime = models.ManyToManyField(Anime)
+    
+    def __str__(self):
+        return self.title
+
