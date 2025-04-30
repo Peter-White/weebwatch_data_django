6d432a5e0e5cfbf1a9062c9b0a4ae094f16a0422
Peter-White <pwhitedeveloper@gmail.com>
2025-04-30 16:48:05 -0500

    model props added

--- showtime/models.py
+++ showtime/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from anime.models import Anime
+from theater.models import Theater
 
-# Create your models here.
+class Showtime(models.Model):
+    theater = models.ForeignKey(Theater, verbose_name="Theater", on_delete=models.CASCADE)
+    anime = models.ForeignKey(Anime, verbose_name="Anime", on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
