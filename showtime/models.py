1d093872de9fa0616f7aeb14525923bb4223d869
Peter-White <pwhitedeveloper@gmail.com>
2025-04-30 20:29:48 -0500

    extra field added

--- showtime/models.py
+++ showtime/models.py
@@ -6,3 +6,4 @@ class Showtime(models.Model):
     theater = models.ForeignKey(Theater, verbose_name="Theater", on_delete=models.CASCADE)
     anime = models.ForeignKey(Anime, verbose_name="Anime", on_delete=models.CASCADE)
     date_time = models.DateTimeField()
+    extras = models.CharField(max_length=25)
